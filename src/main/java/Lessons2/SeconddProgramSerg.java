f8885e0df4dddfbe8d6af0976fd770bfd11222ed
Сергей <arhimed_serg@rambler.ru>
2021-11-07 19:53:35 +0300

    перввая прога, исправленная Вторая прога

--- src/main/java/Lessons2/SeconddProgramSerg.java
+++ src/main/java/Lessons2/SeconddProgramSerg.java
@@ -1,12 +1,13 @@
 package Lessons2;
+
 // Программа высчитывает углы треугольника по изввестным сторонам
 public class SeconddProgramSerg {
 
     public static void main(String[] args) {
 
-        double sideA = 7; // длинна стороныы AВ
-        double sideB = 8; // длинна стороныы ВС
-        double sideC = 10; // длинна стороныы AС
+        double sideA = 3; // длинна стороныы AВ
+        double sideB = 4; // длинна стороныы ВС
+        double sideC = 5; // длинна стороныы AС
 
         double injection1 = cosAB(sideA, sideB, sideC); // угол треугольника A
         System.out.println("Угол A = " + injection1 + " Град.");
@@ -15,24 +16,25 @@ public class SeconddProgramSerg {
         double injection3 = cosAC(sideA, sideB, sideC); // угол треугольника B
         System.out.println("Угол С = " + injection3 + " Град.");
         double exam = injection1 + injection2 + injection3;
-        System.out.println("Провверка суммы угловв  = " + exam + " Град.");
+        System.out.println("Провверка суммы углов  = " + exam + " Град.");
 
     }
 
     public static double cosAB(double arg1, double arg2, double arg3) {
 
-        return Math.toDegrees (Math.acos((((arg1*arg1)+(arg2*arg2)-(arg3*arg3))/(2*arg1*arg2))));
+        return Math.toDegrees(Math.acos(((Math.pow(arg1, 2)) + (Math.pow(arg2, 2)) - (Math.pow(arg3, 2))) / (2 * arg1 * arg2)));
 
     }
+
     public static double cosBC(double arg1, double arg2, double arg3) {
 
-        return Math.toDegrees (Math.acos((((arg2*arg2)+(arg3*arg3)-(arg1*arg1))/(2*arg2*arg3))));
+        return Math.toDegrees(Math.acos(((Math.pow(arg2, 2)) + (Math.pow(arg3, 2)) - (Math.pow(arg1, 2))) / (2 * arg2 * arg3)));
 
     }
 
     public static double cosAC(double arg1, double arg2, double arg3) {
 
-        return Math.toDegrees (Math.acos((((arg1*arg1) + (arg3*arg3) - (arg2*arg2)) / (2 * arg1 * arg3))));
+        return Math.toDegrees(Math.acos(((Math.pow(arg1, 2)) + (Math.pow(arg3, 2)) - (Math.pow(arg2, 2))) / (2 * arg1 * arg3)));
 
     }
 }
