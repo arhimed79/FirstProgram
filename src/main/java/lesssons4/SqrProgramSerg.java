b23ab5d166fcf9ed1b662022128251b6c9000949
Сергей <arhimed_serg@rambler.ru>
2021-11-10 00:45:19 +0300

    перввая прога, исправленная Вторая прога Третья прога 4-я прога

---
+++ src/main/java/lesssons4/SqrProgramSerg.java
@@ -0,0 +1,19 @@
+package lesssons4;
+
+public class SqrProgramSerg {
+    public static void main(String[] args) {
+        int num = 4097;
+        int value = 1;
+        int sum = 0;
+
+        for (int i = 0; value < num; i++) {
+            value = i * i;
+            sum = i;
+        }
+        if (value == num) {
+            System.out.println("корень из " +num+" = "+ sum);
+        } else {
+            System.out.println("корень из " +num+ " не целое число, отввет лежит в диапазоне от "+ (sum - 1) + " до " + sum);
+        }
+    }
+}
