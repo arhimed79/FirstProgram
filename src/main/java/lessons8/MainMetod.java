f43d1a20ea6fba3b4bd0873668ebeb29c8ad3ad8
Сергей <arhimed_serg@rambler.ru>
2021-11-24 23:33:44 +0300

    8-я прога

--- src/main/java/lessons8/MainMetod.java
+++ src/main/java/lessons8/MainMetod.java
@@ -9,8 +9,8 @@ public class MainMetod {
 
         Animals[] animals = new Animals[]{murzik, kesha, shsrik, sherhun};
         for (Animals animals1:animals){
-            animals1.name();
-            animals1.voice();
+            animals1.name(); // Можно объединить в один метод, но в т.з. сказано вывести  voice (голос
+            animals1.voice();// Можно объединить в один метод, но в т.з. сказано вывести  voice (голос
 
             System.out.println("------------");
         }
