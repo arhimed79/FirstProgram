83cf94df72cdff3192f7805d2b9c831227d6af7c
Сергей <arhimed_serg@rambler.ru>
2021-11-29 13:36:52 +0300

    9-я прога

--- src/main/java/lessons9/AutoMobile.java
+++ src/main/java/lessons9/AutoMobile.java
@@ -13,16 +13,15 @@ public abstract class AutoMobile {
 
     private int dRandom() {
         int r = Math.round((float) Math.random() * 1000);
-       return r;
+        return r;
     }
 
     public void move() {
         int i = dRandom();
         if (i <= distance) {
-            System.out.print("   Машина проедет  "+ i + "км. из "+distance+ "км. \n");
-        }else {
-            System.out.print("   Машина не проедет  "+ i + "км. из "+distance+ "км. \n");
+            System.out.print("   Машина проедет  " + i + "км. из " + distance + "км. \n");
+        } else {
+            System.out.print("   Машина не проедет  " + i + "км. из " + distance + "км. \n");
         }
     }
-
 }
