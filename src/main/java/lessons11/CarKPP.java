6d15aa5f4d28e0ccdf57b7b4a7d3083288ac2172
Сергей <arhimed_serg@rambler.ru>
2021-12-09 15:12:41 +0300

    Д.З 11

--- src/main/java/lessons11/CarKPP.java
+++ src/main/java/lessons11/CarKPP.java
@@ -7,7 +7,6 @@ public final class CarKPP extends AutoMobileKPP {
         super(nambe, speed, weight, width, high);
     }
 
-
     @Override
     public void travelKPP() {
         System.out.println("Проверка легкового транспорта");
