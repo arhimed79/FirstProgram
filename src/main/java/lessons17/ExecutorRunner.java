2afdd3f424171928a2fafa8110822f82cb124a57
Сергей <arhimed_serg@rambler.ru>
2022-01-03 02:39:59 +0300

    д. з. 17

---
+++ src/main/java/lessons17/ExecutorRunner.java
@@ -0,0 +1,53 @@
+package lessons17;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class ExecutorRunner {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+
+        int ARRAYSIZE = 1000; // количество значений в коллекции
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        ArrayList<Integer> myList = new ArrayList<>();
+        for (int i = 0; i <= ARRAYSIZE; i++) {
+            myList.add(getValue());
+        }
+        System.out.println(myList.toString());
+
+        Future<Integer> min = executorService.submit(() -> {
+            return Collections.min(myList);
+        });
+        Future<Integer> max = executorService.submit(() -> {
+            return Collections.max(myList);
+        });
+        Future<Double> average = executorService.submit(() -> {
+            int sum = 0;
+            for (int i : myList) {
+                sum += i;
+            }
+            return (sum / (double) myList.size());
+        });
+
+        while (!min.isDone() && !max.isDone() && !average.isDone()) {
+            Thread.sleep(100);
+            System.out.println("Работа потоков не закончена, Ждем");
+        }
+
+        System.out.println(min.get() + " - минимальное значение в коллекции \n" + max.get() + " - максимальное значение в коллекции \n"
+                +average.get()+ " - среднее значение в коллекции");
+        executorService.shutdown();
+    }
+
+    private static int getValue() {
+        Random random = new Random();
+        return random.nextInt(1000);
+
+    }
+}
