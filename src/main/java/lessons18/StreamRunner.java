3d13c354551cba4a41347b6f949dbc785427ced8
Сергей <arhimed_serg@rambler.ru>
2022-01-05 22:00:57 +0300

    д. з. 18

--- src/main/java/lessons18/StreamRunner.java
+++ src/main/java/lessons18/StreamRunner.java
@@ -1,28 +1,42 @@
 package lessons18;
 
 import lombok.SneakyThrows;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class StreamRunner {
 
     @SneakyThrows
     public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
 
-        try (FileReader fileReader = new FileReader("C:\\Users\\Сергей\\IdeaProjects\\FirstProgram\\src\\main\\resources\\lesss18Input.txt")) {
-            try (BufferedReader reader = new BufferedReader(fileReader)) {
-                String lineVerse = reader.readLine();
-                int i = 1;
-                while (lineVerse != null) {
-                    try (FileWriter writerVerse = new FileWriter("C:\\temp_Less18\\Дом_задание_18_Строка - " + i + ".txt")) {
-                        writerVerse.write(lineVerse);
+        executorService.submit(() -> {
+            try (FileReader fileReader = new FileReader("C:\\Users\\Сергей\\IdeaProjects\\FirstProgram\\src\\main\\resources\\lesss18Input.txt")) {
+                try (BufferedReader reader = new BufferedReader(fileReader)) {
+                    String lineVerse = reader.readLine();
+                    int i = 1;
+  //                  System.out.println("Работает поток "+Thread.currentThread().getName());
+                    while (lineVerse != null) {
+                        try (FileWriter writerVerse = new FileWriter("C:\\temp_Less18\\Дом_задание_18_Строка - " + i + ".txt")) {
+                            writerVerse.write(lineVerse);
+                        }
+                        i++;
+                        lineVerse = reader.readLine();
                     }
-                    //  System.out.println(lineVerse);
-                    i++;
-                    lineVerse = reader.readLine();
                 }
+            }catch (Exception e) {
+                System.out.println("Ошибка чтения из файла");
             }
-        }
+        });
+        executorService.shutdown();
+ //       while (!executorService.isTerminated()){
+  //          System.out.println("Поток  "+Thread.currentThread().getName()+" спит");
+  //          Thread.sleep(10);
+   //     }
+  //      System.out.println("Работает поток "+Thread.currentThread().getName());
     }
 }
